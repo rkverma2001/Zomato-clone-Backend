8163fff00c7867f0c3c7e3c16797a885eae07a9d
Ritesh Kumar <riteshkumarverma2018@gmail.com>
2024-06-21 23:49:57 +0530

    Final Commit

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,11 +1,11 @@
 module.exports = {
-    firebaseConfig: {
-        apiKey: "AIzaSyCm7EoD8ibnw7GeJONU9K9QZh8iumuHKns",
-        authDomain: "zomato-clone-fa2d1.firebaseapp.com",
-        databaseURL: 'https://zomato-clone-fa2d1-default-rtdb.asia-southeast1.firebasedatabase.app/',
-        projectId: "zomato-clone-fa2d1",
-        storageBucket: "zomato-clone-fa2d1.appspot.com",
-        messagingSenderId: "264955894111",
-        appId: "1:264955894111:web:7cc29f32f94fcce914feea"
-    }
-}
+  firebaseConfig: {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+  },
+};
