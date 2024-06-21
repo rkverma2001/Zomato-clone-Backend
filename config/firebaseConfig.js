6d10c430791ff4ee6ee9670f79b81d850dc12b31
Ritesh Kumar <riteshkumarverma2018@gmail.com>
2024-06-21 23:36:29 +0530

    Zomato Clone API Created

---
+++ config/firebaseConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+    firebaseConfig: {
+        apiKey: "AIzaSyCm7EoD8ibnw7GeJONU9K9QZh8iumuHKns",
+        authDomain: "zomato-clone-fa2d1.firebaseapp.com",
+        databaseURL: 'https://zomato-clone-fa2d1-default-rtdb.asia-southeast1.firebasedatabase.app/',
+        projectId: "zomato-clone-fa2d1",
+        storageBucket: "zomato-clone-fa2d1.appspot.com",
+        messagingSenderId: "264955894111",
+        appId: "1:264955894111:web:7cc29f32f94fcce914feea"
+    }
+}
